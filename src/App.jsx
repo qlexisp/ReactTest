7488b987c3931afd789ef216f8b03086b64ca9bc
alex <1alexispetropoulos@gmail.com>
2024-04-23 15:16:10 +0200

    exercice des listes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
 import { useState } from 'react'
 import './App.css'
-import Article from './Article.jsx'
+import { people } from './data.js';
+import { getImageUrl } from './utils.js';
+//import Article from './Article.jsx'
 
 export default function App() {
 
-  return (
-    <>
-      <h2>Liste des albums:</h2>
-      <Article />
-    </>
+  const listChimist = people.filter(person => person.profession === 'chimiste')
+  const displayChimist = listChimist.map(person =>
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        célèbre pour {person.accomplishment}
+      </p>
+    </li>
   )
+
+  const listItems = people.filter(person => person.profession != 'chimiste')
+  const displayPeople = listItems.map(person =>
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        célèbre pour {person.accomplishment}
+      </p>
+    </li>
+  );
+  return (
+    <article>
+      <h1>Chimistes:</h1>
+      <ul>{displayChimist}</ul>
+
+      <h1>Autres:</h1>
+      <ul>{displayPeople}</ul>
+    </article>
+  );
 }
