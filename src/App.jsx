b3de5efa508da3a12f187a8dae494b30df41e345
alex <1alexispetropoulos@gmail.com>
2024-04-23 15:09:20 +0200

    tableaux

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,8 @@ export default function App() {
 
   return (
     <>
-      <h2>Mes articles:</h2>
-      <Article title="Titre du premier article"><p>Premier article</p></Article>
-      <Article title="Titre du deuxième article"><p>Deuxième article</p></Article>
+      <h2>Liste des albums:</h2>
+      <Article />
     </>
   )
 }
